044c860743b4e544b7ab02bcdf2328bcfa79a4cb
Pedro Henrique Camargo Ruthes <pedroo.ruthes@gmail.com>
2025-11-24 15:28:23 -0300

    feat: add Input component

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,7 +1,9 @@
-import { View, Button } from "react-native";
+import { View } from "react-native";
 import { router } from "expo-router";
 
 import { PageHeader } from "@/components/PageHeader";
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
 
 export default function Target() {
   return (
@@ -10,7 +12,14 @@ export default function Target() {
         title="Meta"
         subtitle="Economize para alcançar sua meta financeira."
       />
-      <Button title="Voltar" onPress={() => router.back()} />
+
+      <View style={{ marginTop: 32, gap: 24 }}>
+        <Input
+          label="Nome da meta"
+          placeholder="Ex: Viagem para praia, Apple Watch"
+        />
+        <Button title="Salvar" />
+      </View>
     </View>
   );
 }
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,22 @@
+import { View, TextInput, TextInputProps, Text } from "react-native";
+
+import { styles } from "./styles";
+import { colors } from "@/theme";
+
+type Props = TextInputProps & {
+  label: string;
+};
+
+export function Input({ label, ...rest }: Props) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholderTextColor={colors.gray[400]}
+        {...rest}
+      />
+    </View>
+  );
+}
