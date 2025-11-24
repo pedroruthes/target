0463542d87cfa4306787bf7f330875b4684b90ba
Pedro Henrique Camargo Ruthes <pedroo.ruthes@gmail.com>
2025-11-24 10:42:26 -0300

    fix: theme index.ts imports

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,3 +1,4 @@
+import { View, Text } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import { styles } from "./styles";
@@ -8,6 +9,10 @@ export function HomeHeader() {
     <LinearGradient
       colors={[colors.blue[500], colors.blue[800]]}
       style={styles.container}
-    ></LinearGradient>
+    >
+      <View>
+        <Text style={styles.label}>Total que você possui</Text>
+      </View>
+    </LinearGradient>
   );
 }
