493ae1cf9f9327d7366e7668a355f92e9ba232fd
Pedro Henrique Camargo Ruthes <pedroo.ruthes@gmail.com>
2025-11-24 15:48:58 -0300

    feat: add Progress component

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,14 +1,29 @@
-import { View, Text, Button } from "react-native";
-import { router, useLocalSearchParams } from "expo-router";
+import { View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+
+import { PageHeader } from "@/components/PageHeader";
+import { Progress } from "@/components/Progress";
+
+const details = {
+  current: "R$ 580,00",
+  target: "R$ 1.790,00",
+  percentage: 25,
+};
 
 export default function InProgress() {
   const params = useLocalSearchParams<{ id: string }>();
 
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text>ID In Progress: {params.id}</Text>
+    <View style={{ flex: 1, padding: 24, gap: 32 }}>
+      <PageHeader
+        title="Apple Watch"
+        rightButton={{
+          icon: "edit",
+          onPress: () => {},
+        }}
+      />
 
-      <Button title="Voltar" onPress={() => router.back()} />
+      <Progress data={details} />
     </View>
   );
 }
