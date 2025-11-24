5a4ec58d0cb4730bcc1c8be15fd429bedb5233f0
Pedro Henrique Camargo Ruthes <pedroo.ruthes@gmail.com>
2025-11-24 11:08:01 -0300

    feat: add Summary component

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -5,6 +5,7 @@ import { styles } from "./styles";
 import { colors } from "@/theme/colors";
 
 import { Separator } from "../Separator";
+import { Summary } from "../Summary";
 
 export type HomeHeaderProps = {
   total: string;
@@ -26,6 +27,19 @@ export function HomeHeader({ data }: Props) {
       </View>
 
       <Separator color={colors.blue[400]} />
+
+      <View style={styles.summary}>
+        <Summary
+          data={{ label: "Entradas", value: "R$ 6.184,90" }}
+          icon={{ name: "arrow-upward", color: colors.green[500] }}
+        />
+
+        <Summary
+          data={{ label: "Saídas", value: "-R$ 883,65" }}
+          icon={{ name: "arrow-downward", color: colors.red[400] }}
+          isLeft
+        />
+      </View>
     </LinearGradient>
   );
 }
