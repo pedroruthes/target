8b33ddde4fa2a22677c156e350453385b4eecc66
Pedro Henrique Camargo Ruthes <pedroo.ruthes@gmail.com>
2025-11-24 16:04:52 -0300

    feat: add Transaction component

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -3,12 +3,32 @@ import { useLocalSearchParams } from "expo-router";
 
 import { PageHeader } from "@/components/PageHeader";
 import { Progress } from "@/components/Progress";
+import { List } from "@/components/List";
+import { Transaction, TransactionProps } from "@/components/Transaction";
+
+import { TransactionTypes } from "@/utils/TransactionTypes";
 
 const details = {
   current: "R$ 580,00",
   target: "R$ 1.790,00",
   percentage: 25,
 };
+
+const transactions: TransactionProps[] = [
+  {
+    id: "1",
+    value: "R$ 20,00",
+    date: "12/04/25",
+    type: TransactionTypes.Output,
+  },
+  {
+    id: "2",
+    value: "R$ 300,00",
+    date: "12/04/25",
+    description: "CDB de 110% no banco XPTO",
+    type: TransactionTypes.Input,
+  },
+];
 
 export default function InProgress() {
   const params = useLocalSearchParams<{ id: string }>();
@@ -24,6 +44,14 @@ export default function InProgress() {
       />
 
       <Progress data={details} />
+
+      <List
+        title="Transações"
+        data={transactions}
+        renderItem={({ item }) => (
+          <Transaction data={item} onRemove={() => {}} />
+        )}
+      />
     </View>
   );
 }
---
+++ src/components/Transaction/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+import { colors, fontFamily } from "@/theme";
+
+export const styles = StyleSheet.create({
+  container: {
+    height: 72,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 7,
+  },
+  info: {
+    flex: 1,
+    gap: 7,
+  },
+  value: {
+    fontSize: 14,
+    fontFamily: fontFamily.medium,
+    color: colors.black,
+  },
+  description: {
+    fontSize: 12,
+    fontFamily: fontFamily.regular,
+    color: colors.gray[500],
+  },
+});
