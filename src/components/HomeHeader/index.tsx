ba946ef4a795777d48644c95023581ad862668f7
Pedro Henrique Camargo Ruthes <pedroo.ruthes@gmail.com>
2025-11-24 10:48:33 -0300

    feat: add Separator component

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -4,7 +4,17 @@ import { LinearGradient } from "expo-linear-gradient";
 import { styles } from "./styles";
 import { colors } from "@/theme/colors";
 
-export function HomeHeader() {
+import { Separator } from "../Separator";
+
+export type HomeHeaderProps = {
+  total: string;
+};
+
+type Props = {
+  data: HomeHeaderProps;
+};
+
+export function HomeHeader({ data }: Props) {
   return (
     <LinearGradient
       colors={[colors.blue[500], colors.blue[800]]}
@@ -12,7 +22,10 @@ export function HomeHeader() {
     >
       <View>
         <Text style={styles.label}>Total que você possui</Text>
+        <Text style={styles.total}>{data.total}</Text>
       </View>
+
+      <Separator color={colors.blue[400]} />
     </LinearGradient>
   );
 }
