7f0b435c6d333a82c6d193b9aa7fd5d4b6507ecf
Pedro Henrique Camargo Ruthes <pedroo.ruthes@gmail.com>
2025-11-24 11:33:36 -0300

    feat: add Target component

---
+++ src/components/Target/index.tsx
@@ -0,0 +1,39 @@
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { styles } from "./styles";
+
+export type TargetProps = {
+  id?: string;
+  name: string;
+  percentage: string;
+  current: string;
+  target: string;
+};
+
+type Props = TouchableOpacityProps & {
+  data: TargetProps;
+};
+
+export function Target({ data, ...rest }: Props) {
+  return (
+    <TouchableOpacity style={styles.container} {...rest}>
+      <View style={styles.content}>
+        <Text style={styles.name} numberOfLines={1}>
+          {data.name}
+        </Text>
+
+        <Text style={styles.status}>
+          {data.percentage} • {data.current} de {data.target}
+        </Text>
+      </View>
+
+      <MaterialIcons name="chevron-right" size={20} />
+    </TouchableOpacity>
+  );
+}
