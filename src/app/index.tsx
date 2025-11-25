792e360119dac54ac5c17891e86339c89eabd516
Pedro Henrique Camargo Ruthes <pedroo.ruthes@gmail.com>
2025-11-25 10:08:12 -0300

    feat: add listBySavedValue and show functions database

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,16 @@
-import { View, StatusBar } from "react-native";
-import { router } from "expo-router";
+import { useCallback, useState } from "react";
+import { View, StatusBar, Alert } from "react-native";
+import { router, useFocusEffect } from "expo-router";
 
 import { HomeHeader } from "@/components/HomeHeader";
-import { Target } from "@/components/Target";
+import { Target, TargetProps } from "@/components/Target";
 import { List } from "@/components/List";
 import { Button } from "@/components/Button";
+import { Loading } from "@/components/Loading";
+
+import { numberToCurrency } from "@/utils/numberToCurrency";
+
+import { useTargetDatabase } from "@/database/useTargetDatabase";
 
 const summary = {
   total: "R$ 2.680,00",
@@ -12,31 +18,47 @@ const summary = {
   output: { label: "Saídas", value: "-R$ 883,65" },
 };
 
-const targets = [
-  {
-    id: "1",
-    name: "Apple Watch",
-    percentage: "50%",
-    current: "R$ 580,00",
-    target: "R$ 1.790,00",
-  },
-  {
-    id: "2",
-    name: "Comprar uma cadeira ergonômica",
-    percentage: "75%",
-    current: "R$ 900,00",
-    target: "R$ 1.200,00",
-  },
-  {
-    id: "3",
-    name: "Fazer uma viagem para o Rio de Janeiro",
-    percentage: "75%",
-    current: "R$ 1.200,00",
-    target: "R$ 3.000,00",
-  },
-];
-
 export default function Index() {
+  const [isFetching, setIsFetching] = useState(true);
+  const [targets, setTargets] = useState<TargetProps[]>([]);
+
+  const targetDatabase = useTargetDatabase();
+
+  async function fetchTargets(): Promise<TargetProps[]> {
+    try {
+      const response = await targetDatabase.listBySavedValue();
+
+      return response.map((item) => ({
+        id: String(item.id),
+        name: item.name,
+        current: numberToCurrency(item.current),
+        percentage: item.percentage.toFixed(0) + "%",
+        target: numberToCurrency(item.amount),
+      }));
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possível carregar as metas.");
+      console.log(error);
+    }
+  }
+
+  async function fetchData() {
+    const targetDataPromise = fetchTargets();
+    const [targetData] = await Promise.all([targetDataPromise]);
+
+    setTargets(targetData);
+    setIsFetching(false);
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchData();
+    }, [])
+  );
+
+  if (isFetching) {
+    return <Loading />;
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <StatusBar barStyle="light-content" />
--- src/database/useTargetDatabase.ts
+++ src/database/useTargetDatabase.ts
@@ -5,6 +5,16 @@ export type TargetCreate = {
   amount: number;
 };
 
+export type TargetResponse = {
+  id: number;
+  name: string;
+  amount: number;
+  current: number;
+  percentage: number;
+  created_at: Date;
+  updated_at: Date;
+};
+
 export function useTargetDatabase() {
   const database = useSQLiteContext();
 
@@ -19,7 +29,42 @@ export function useTargetDatabase() {
     });
   }
 
+  function listBySavedValue() {
+    return database.getAllAsync<TargetResponse>(`
+      SELECT
+        targets.id,
+        targets.name,
+        targets.amount,
+        COALESCE (SUM(transactions.amount), 0) AS current,
+        COALESCE ((SUM(transactions.amount) / targets.amount) * 100, 0) AS percentage,
+        targets.created_at,
+        targets.updated_at
+      FROM targets
+      LEFT JOIN transactions ON targets.id = transactions.target_id
+      GROUP BY targets.id, targets.name, targets.amount
+      ORDER BY current DESC
+    `);
+  }
+
+  function show(id: number) {
+    return database.getFirstAsync<TargetResponse>(`
+      SELECT
+        targets.id,
+        targets.name,
+        targets.amount,
+        COALESCE (SUM(transactions.amount), 0) AS current,
+        COALESCE ((SUM(transactions.amount) / targets.amount) * 100, 0) AS percentage,
+        targets.created_at,
+        targets.updated_at
+      FROM targets
+      LEFT JOIN transactions ON targets.id = transactions.target_id
+      WHERE targets.id = ${id}
+    `);
+  }
+
   return {
     create,
+    show,
+    listBySavedValue,
   };
 }
