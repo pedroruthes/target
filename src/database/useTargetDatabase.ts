534b22dd3d36017c8ab88ea0f7cba27d8febdbab
Pedro Henrique Camargo Ruthes <pedroo.ruthes@gmail.com>
2025-11-25 10:38:39 -0300

    feat: add remove target function

--- src/database/useTargetDatabase.ts
+++ src/database/useTargetDatabase.ts
@@ -82,10 +82,15 @@ export function useTargetDatabase() {
     });
   }
 
+  async function remove(id: number) {
+    await database.runAsync("DELETE FROM targets WHERE id = ?", id);
+  }
+
   return {
     create,
     show,
     update,
+    remove,
     listBySavedValue,
   };
 }
